c299d5cd9faf292addc15423b830fde56363359f
Marton Valki <marton.valki@nokia.com>
2018-08-01 16:21:30 +0200

    -

--- visualization/visualizer/views.py
+++ visualization/visualizer/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 import sys
-sys.path.insert(0, '/home/ubuntu/Chargebee/')
+sys.path.insert(0, 'C:/Users/VALKI/Documents/Chargebee')
 import getNames
 
 def table(request):
